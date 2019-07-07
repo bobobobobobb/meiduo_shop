7d37163757fae97fde9ade8159d3ee055d650bb4
iloveyour <744731084@qq.com>
2019-07-07 19:39:48 +0800

    项目搭建

---
+++ meiduo_mall/apps/users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
